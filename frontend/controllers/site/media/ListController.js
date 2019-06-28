d70b920d4a43b09951cfa4ac2a1271a0f7cd2ab1
Tara Williams <taraewilliams12@gmail.com>
2019-06-28 19:24:33 -0400

    Refactoring code for ListController page.

--- frontend/controllers/site/media/ListController.js
+++ frontend/controllers/site/media/ListController.js
@@ -11,17 +11,10 @@ app.controller('ListController', function($scope, $routeParams, RequestService, 
         /* The maximum number of pages to show in the pagination bar */
         $scope.maxPages = 5;
 
+        /* The limit for 'all' is set to 0 */
+        var all_limit = 0;
         var offset = $routeParams.offset;
         var limit = $routeParams.limit;
-
-        /* Set the maximum number of items per page */
-        if (limit == 0){
-            $scope.maxPerPage = "all";
-        }else{
-            $scope.maxPerPage = limit;
-        }
-        /* Set the current page offset */
-        $scope.currentOffset = offset;
 
         /* Set the variables for the books/movies/games */
         if ($scope.isActive('/books/:offset/:limit')){
@@ -32,7 +25,7 @@ app.controller('ListController', function($scope, $routeParams, RequestService, 
                 delete_url: CONFIG.api + '/books/',
                 delete_text: "Delete this book?"
             };
-            if (offset == 0 && limit == 0){
+            if (limit == all_limit){
                 $scope.variables.get_url = CONFIG.api + '/books';
             }
         } else if ($scope.isActive('/movies/:offset/:limit')) {
@@ -43,7 +36,7 @@ app.controller('ListController', function($scope, $routeParams, RequestService, 
                 delete_url: CONFIG.api + '/movies/',
                 delete_text: "Delete this movie?"
             };
-            if (offset == 0 && limit == 0){
+            if (limit == all_limit){
                 $scope.variables.get_url = CONFIG.api + '/movies';
             }
         }else{
@@ -54,56 +47,36 @@ app.controller('ListController', function($scope, $routeParams, RequestService, 
                 delete_url: CONFIG.api + '/games/',
                 delete_text: "Delete this game?"
             };
-            if (offset == 0 && limit == 0){
+            if (limit == all_limit){
                 $scope.variables.get_url = CONFIG.api + '/games';
             }
         }
 
-        /* Get the items to display on the page */
+        $scope.maxPerPage = setMaxPerPage(offset, limit, all_limit);
+        $scope.currentOffset = offset;
+
+        /* Get the media items to display on the page */
         $http.get($scope.variables.get_url)
         .then(function(response) {
-            $scope.items = response.data;
+            $scope.media = response.data;
         });
 
-        if ($scope.maxPerPage != "all"){
-
-            /* Count all the items */
+        if ($scope.maxPerPage != "all" && $scope.maxPerPage != "invalid"){
+            /* Count all the media items */
             $http.get($scope.variables.get_count_url)
             .then(function(response) {
-                var num_items = parseInt(response.data.num);
-                var num_pages = Math.ceil(num_items/limit);
-
-                var current_page = (offset/limit) + 1;
-                if ((current_page + $scope.maxPages - 1) >= num_pages){
-                    var last_page = num_pages;
-                    var first_page = (num_pages - $scope.maxPages) + 1;
-                }else{
-                    var first_page = current_page;
-                    var last_page = (current_page + $scope.maxPages) - 1;
-                }
-
-                $scope.pages = [];
-                for(i = 1; i <= num_pages; i++){
-                    var page = {
-                        num: i,
-                        offset: (i - 1) * limit,
-                        limit: limit,
-                        active: i == current_page,
-                        current: i >= first_page && i <= last_page,
-                        firstPage: i == first_page,
-                        lastPage: i == last_page
-                    };
-                    $scope.pages.push(page);
-                }
+                var num_pages = Math.ceil(parseInt(response.data.num)/limit);
+                $scope.pages = makePages(num_pages, offset, limit);
             });
         }
     }
 
-    /* Delete an item for its ID */
-    $scope.deleteItem = function(itemID){
+    /* Delete a media item for its ID */
 
+    $scope.deleteMedia = function(mediaID)
+    {
         if (confirm($scope.variables.delete_text)){
-            var url = $scope.variables.delete_url + itemID;
+            var url = $scope.variables.delete_url + mediaID;
 
             $http.delete(url)
             .then(function(response) {
@@ -120,24 +93,17 @@ app.controller('ListController', function($scope, $routeParams, RequestService, 
         $scope.goToPath(path + page.offset + "/" + page.limit);
     };
 
-    $scope.skipOnePage = function(skip, pages, path){
-        var active_page = getActivePage(pages);
-
+    $scope.skipPage = function(skip, pages, path){
         if(skip == "forward"){
-            active_page = (active_page + 1 >= pages.length) ? pages.length : active_page + 1;
+            active_page = ($scope.current_page + 1 >= pages.length) ? pages.length : $scope.current_page + 1;
+        }else if (skip == "backward"){
+            active_page = ($scope.current_page - 1 <= 1) ? 1 : $scope.current_page - 1;
+        }else if (skip == "forward_final"){
+            active_page = pages.length;
         }else{
-            active_page = (active_page - 1 <= 1) ? 1 : active_page - 1;
+            active_page = 1;
         }
         var page = getPage(active_page, pages);
-        $scope.goToPath(path + page.offset + "/" + page.limit);
-    };
-
-    $scope.skipToFinalPage = function(skip, pages, path){
-        if(skip == "forward"){
-            var page = getPage(pages.length, pages);
-        }else{
-            var page = getPage(1, pages);
-        }
         $scope.goToPath(path + page.offset + "/" + page.limit);
     };
 
@@ -152,21 +118,61 @@ app.controller('ListController', function($scope, $routeParams, RequestService, 
 
     /* Private Functions */
 
-    var getActivePage = function(pages){
-        for (i = 0; i < pages.length; i++){
-            var page = pages[i];
-            if (page.active){
-                return page.num;
+    var getPage = function(page_num, pages){
+        return pages[page_num - 1];
+    };
+
+    var makePages = function(num_pages, offset, limit){
+
+        $scope.current_page = (offset/limit) + 1;
+        if (($scope.current_page + $scope.maxPages - 1) >= num_pages){
+            var last_page = num_pages;
+            var first_page = (num_pages - $scope.maxPages) + 1;
+        }else{
+            var first_page = $scope.current_page;
+            var last_page = ($scope.current_page + $scope.maxPages) - 1;
+        }
+
+        var temp_pages = [];
+        for(i = 1; i <= num_pages; i++){
+            var page = {
+                num: i,
+                offset: (i - 1) * limit,
+                limit: limit,
+                active: i == $scope.current_page,
+                visible: i >= first_page && i <= last_page,
+                firstPage: i == first_page,
+                lastPage: i == last_page
+            };
+            temp_pages.push(page);
+        }
+        return temp_pages;
+    };
+
+    var setMaxPerPage = function(offset, limit, all_limit){
+        if (isValidLimit(limit) && isValidOffset(offset, limit)){
+            if (limit == all_limit){
+                return "all";
+            }else{
+                return limit;
             }
+        }else{
+            var new_limit = 25;
+            var new_offset = Math.floor(offset/new_limit) * new_limit;
+            $scope.goToPath($scope.variables.path + new_offset + "/" + new_limit);
+            return "invalid";
         }
     };
 
-    var getPage = function(page_num, pages){
-        for (i = 0; i < pages.length; i++){
-            var page = pages[i];
-            if (page.num == page_num){
-                return page;
-            }
+    var isValidLimit = function(limit){
+        return (limit == 0 || limit == 25 || limit == 50 || limit == 75 || limit == 100);
+    };
+
+    var isValidOffset = function(offset, limit){
+        if (limit == 0){
+            return offset == 0;
+        }else{
+            return (offset % limit == 0);
         }
     };
 
